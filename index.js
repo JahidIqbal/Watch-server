b0ed63463470b332ec1435fb004819fff2f03432
hrid620 <hridishere@gmail.com>
2021-11-13 02:09:47 +0600

    admin server

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const express = require('express')
 const cors = require('cors')
 const { MongoClient } = require('mongodb');
-require('dotenv').config()
-// const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json());
 
-// DB_USER=JIWatch
-// DB_PASS=cOVjtiVNFgjUpQCu
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sbyjk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })`;
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sbyjk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 console.log(uri);
 
@@ -25,6 +24,8 @@ async function run() {
         const database = client.db("JIWatch");
         const servicesCollection = database.collection("services");
         console.log('database connected')
+        const orderCollection = database.collection("orders");
+        const usersCollection = database.collection('users');
 
         app.get('/services', async (req, res) => {
             const cursor = servicesCollection.find({});
@@ -32,7 +33,77 @@ async function run() {
             res.send(services);
         })
 
+        //Add Orders API
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order)
+            res.send(result);
+        })
+
+        // get
+        app.get('/orders', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const cursor = orderCollection.find(query)
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            console.log('delete order with id', result);
+            res.json(result);
+        })
+
+
+        // post users
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        })
+
+
+        // put method
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+
+        // admin role
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+
+        })
+
+        // finding specific admin
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin })
+        })
+
+
+
     }
+
 
     finally {
         //   await client.close();
